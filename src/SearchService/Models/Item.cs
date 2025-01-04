dfda07a697bd84f0289b8d913c58cb4ff75a4da9
edensquall <edensquall@msn.com>
2025-01-04 22:20:13 +0800

    End of section 3

--- src/SearchService/Models/Item.cs
+++ src/SearchService/Models/Item.cs
@@ -1,15 +1,16 @@
-namespace AuctionService.DTOs;
+using MongoDB.Entities;
 
-public class AuctionDto
+namespace SearchService.Models;
+
+public class Item : Entity
 {
-  public Guid Id { get; set; }
   public int ReservePrice { get; set; }
   public string Seller { get; set; }
   public string Winner { get; set; }
   public int SoldAmount { get; set; }
   public int CurrentHighBid { get; set; }
   public DateTime CreatedAt { get; set; }
-  public DateTime UpdateAt { get; set; }
+  public DateTime UpdatedAt { get; set; }
   public DateTime AuctionEnd { get; set; }
   public string Status { get; set; }
   public string Make { get; set; }
